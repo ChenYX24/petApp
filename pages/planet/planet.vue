92a987b4db6ccd846d349f03ccd46d3f2340e3a7
VAN2for <90973590+2389927630@users.noreply.github.com>
2023-03-23 23:18:45 +0800

    planet的css完善

--- pages/planet/planet.vue
+++ pages/planet/planet.vue
@@ -6,6 +6,15 @@
 			<input type="text" placeholder="请输入ta的名字" class="input" placeholder-style="color:#cea697;" v-model="inputValue">
 			<image src="/static/IDCreate/picture&name/pen.png" mode="aspectFill" ></image>
 		</view>
+		<view class="button2" :class="{ active: isActive }" @tap="nextpage">
+			<image src="/static/IDCreate/picture&name/cat.png" mode="aspectFill" ></image>
+			点击加入{{trueProvince}}星球
+			<image src="/static/IDCreate/picture&name/dog.png" mode="aspectFill" ></image>
+		</view>
+		<view class="advantage">
+			<text>加入星球的四点好处~</text>
+			xxxxxxxxxxxxxxxxxxxxxx
+		</view>
 	</view>
     <!-- 引用自定义tabbar组件 -->
     <tab-bar :activeTab="tab"></tab-bar><!--将 activeTab 绑定到 Vue 实例中的 tab 变量上，tab为true时-->
@@ -26,10 +35,16 @@ export default {
 
   data() {
   	return {
-  		tab: ''
+  		tab: '',
+		inputValue: '',
+		trueProvince:'xx'
   	}
   },
-
+		 computed: {
+		    isActive() {
+		      return this.inputValue.trim() !== '';
+		    }
+		  }
 };
 </script>
 
@@ -57,6 +72,21 @@ export default {
     height: 100vh;
     background-color: #ffb776;
 }
+.advantage{
+	display: flex;
+	
+	width: 90%;
+    height: 80vh;
+    background-color: #efdbd0;
+    position: relative;
+    top: 5.5vh;
+	color: #cea697;
+    border-top-left-radius: 2rem;
+    border-top-right-radius: 2rem;
+    box-shadow: 0px -20px 10px -4px rgb(239 219 208 / 50%);
+    flex-direction: column;
+       align-items: center;
+}
 .inputBox{
 		margin-top: 0px;
 		top: 2vh;
@@ -82,6 +112,33 @@ export default {
 			height: 2rem;
 			z-index: 99;
 		}
+}
+.button2{
+	        margin-top: 0px;
+		    top: 3vh;
+		    width: 85vw;
+		    height: 8vh;
+			background-color:rgba(221,221,221,1) ;
+			border-radius: 5vw;
+			padding: 0.5rem;
+            position: relative;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			margin-top: 1rem;
+			color: #cea697;
+			font-weight: bold;
+			font-size: 13pt;//字体
+		image{
+			width: 5rem;
+			height: 5rem;
+			z-index: 99;
+		}
+		}
+.button2.active{
+	height: 55vh!important;
+	background-color:rgba(252,206,181,0.67)!important ;
+	box-shadow: 11px 15px 24px -9px rgba(249, 189, 125, 0.7);
 }
 
 </style>
